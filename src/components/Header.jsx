e5efd5264bec6ff48c16fe6c01d987e09ef82a45
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-19 14:59:42 -0600

    💄 style: Add initial styles to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => (
-    <div>
-      <h2>Header Component</h2>
-    </div>
-  );
+  <div>
+    <h2 className="mx-auto text-center text-5xl font-black md:w-2/3">
+      Vet Clinic Patients <span className="text-indigo-600">Follow-Up</span>
+    </h2>
+  </div>
+);
 
 export default Header;
