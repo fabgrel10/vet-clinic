081ced4472e4417891e1eab523db5b28f19cfdcf
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-23 18:54:22 -0600

    🔧 feat: Add form and style to Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,84 @@
 const Form = () => (
-  <div className="lg:2/5 md:w-1/2">
-    <h2 className="text-center text-3xl font-black">Patient FollowUps</h2>
-    <p className="mt-5 text-center">
-      Add <span className="font-bold text-indigo-600">Patient</span>
-    </p>
+  <div className=" lg:2/5 md:w-1/2">
+    <h2 className="mb-5 text-center text-3xl font-black">
+      {' '}
+      <span className="font-bold text-indigo-600">Patient</span> Info
+    </h2>
+    <form className="mb-10 rounded-lg bg-white py-10 px-5 shadow-md">
+      <div className="mb-5">
+        <label
+          htmlFor="pet"
+          className="block font-bold uppercase text-gray-700"
+        >
+          Pet Name
+          <input
+            id="pet"
+            type="text"
+            placeholder="Buddy"
+            className="mt-2 w-full rounded-md border-2 p-2 placeholder-gray-400"
+          />
+        </label>
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="owner"
+          className="block font-bold uppercase text-gray-700"
+        >
+          Owner&apos;s Name
+          <input
+            id="owner"
+            type="text"
+            placeholder="John Doe"
+            className="mt-2 w-full rounded-md border-2 p-2 placeholder-gray-400"
+          />
+        </label>
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block font-bold uppercase text-gray-700"
+        >
+          Email
+          <input
+            id="email"
+            type="email"
+            placeholder="j.dow@email.com"
+            className="mt-2 w-full rounded-md border-2 p-2 placeholder-gray-400"
+          />
+        </label>
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="entry"
+          className="block font-bold uppercase text-gray-700"
+        >
+          Entry Date
+          <input
+            id="entry"
+            type="date"
+            className="mt-2 w-full rounded-md border-2 p-2 placeholder-gray-400"
+          />
+        </label>
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="symptoms"
+          className="block font-bold uppercase text-gray-700"
+        >
+          Symptoms
+          <textarea
+            id="symptoms"
+            className="mt-2 w-full rounded-md border-2 p-2 placeholder-gray-400"
+            placeholder="Decreased Appetite ..."
+          />
+        </label>
+      </div>
+      <input
+        type="submit"
+        className="w-full cursor-pointer bg-indigo-600 p-3 font-bold uppercase text-white transition-colors hover:bg-indigo-700"
+        value="Add Patient"
+      />
+    </form>
   </div>
 );
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => (
   <div>
     <h2 className="mx-auto text-center text-5xl font-black md:w-2/3">
-      Vet Clinic Patients <span className="text-indigo-600">Follow-Up</span>
+      Vet Clinic
     </h2>
   </div>
 );
